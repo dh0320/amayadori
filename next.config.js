74991212ffbb28c16c7ab3579c83e554d11c70d6
dh <57335155+dh0320@users.noreply.github.com>
2025-09-05 23:50:00 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  images: { unoptimized: true },
+  // trailingSlash: true, // ルーティング方針に応じて
+};
 module.exports = nextConfig;
